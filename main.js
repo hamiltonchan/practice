a66587f2f28ebb6cc938878adc4b743725950640
Hamilton Chan <hamiltonchanjr@gmail.com>
2014-01-15 15:27:33 -0800

    add images

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function petsCtrl ($scope) {
-	$scope.pets = ["Dr. Doom", "Wilson", "Tuna"]
+	$scope.pets = [
+		{name: "Dr. Doom", image:"http://placekitten.com/150/150", adopted:false},
+		{name: "Wilson", image:"http://placekitten.com/151/150", adopted:false},
+		{name: "Tuna", image:"http://placekitten.com/152/150", adopted:false}
+		];
 }
