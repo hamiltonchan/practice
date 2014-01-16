56ab7eaf92b109fef5803901e365479a25802c48
Hamilton Chan <hamiltonchanjr@gmail.com>
2014-01-15 16:37:09 -0800

    allow input of new kitten

--- main.js
+++ main.js
@@ -4,4 +4,12 @@ function petsCtrl ($scope) {
 		{name: "Wilson", image:"http://placekitten.com/151/150", adopted:false},
 		{name: "Tuna", image:"http://placekitten.com/152/150", adopted:false}
 		];
+
+		$scope.addNewPet = function() {
+			$scope.pets.push({name: $scope.newPetName, image: $scope.newPetImage, adopted: false});
+			$scope.newPetImage = "";
+			$scope.newPetName = "";
+		}
+
 }
+
